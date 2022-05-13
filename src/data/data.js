5c0314b16fd666668bcbec83039746c12180b361
Jiazheng Jin Jia <j.jinjia99@gmail.com>
2022-05-13 13:51:25 +0200

    added full stack project to project data

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,7 @@ import morse from "../assets/images/morse-code.png";
 import punkApi from "../assets/images/brewdog.png";
 import ticketTracker from "../assets/images/ticket-tracker.png";
 import positivePup from "../assets/images/positive-pup.png";
+import fullStackApp from "../assets/images/full-stack-app.png";
 const myProjects = [
   {
     id: 1,
@@ -58,6 +59,15 @@ const myProjects = [
       "An on-going project built with React for a dog training business following a design on Figma. The project focueses on pair programming and agile methodologies with Scrum as well as putting into practice all React fundamentals.  ",
     liveSite: "https://nology-tech.github.io/positive-pup/",
     gitHub: "https://github.com/nology-tech/positive-pup",
+  },
+  {
+    id: 7,
+    image: fullStackApp,
+    title: "Full Stack Application",
+    description:
+      "A full stack website where the front end is built with React and the backend is handled by Java/SpringBoot applications. The API is deployed in Google Cloud Platform(GCP). The theme is a music libary where you can search, add and delete songs and play the song in the youtube preview.  The project aimed to put into practice everything I learned duriing the nology course",
+    liveSite: "https://j-jinjia.github.io/full-stack-project/",
+    gitHub: "https://github.com/j-jinjia/full-stack-project",
   },
 ];
 export default myProjects;
